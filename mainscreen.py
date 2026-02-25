85b3b02c525af7719aa41ea1c9aa052f314ae607
Herambha1226 <pallapothugayathri23@gmail.com>
2026-02-25 18:01:47 +0530

    Few Updates Can Make Frontend

--- mainscreen.py
+++ mainscreen.py
@@ -1,3 +1,4 @@
+from kivymd.uix.list import IconRightWidget
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
@@ -10,7 +11,7 @@ from kivymd.uix.screen import MDScreen
 from kivy.utils import get_color_from_hex
 from kivymd.uix.scrollview import MDScrollView
 from kivymd.uix.list import MDList,OneLineAvatarIconListItem,IconLeftWidget
-
+from kivymd.toast import toast
 class home(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -52,7 +53,8 @@ class home(MDScreen):
             icon="plus",
             pos_hint = {"center_x":0.5,"center_y":0.5,'x':0.5},
             theme_text_color="Custom",
-            text_color=get_color_from_hex("#A4A5A6")
+            text_color=get_color_from_hex("#A4A5A6"),
+            on_release=self.move_to_all_users
         )
 
         # Scroll View Adding 
@@ -60,9 +62,9 @@ class home(MDScreen):
 
         self.list = MDList()
 
-        friends = ["self(You)"]
+        self.friends = ["self(You)"]
 
-        for name in friends:
+        for name in self.friends:
             item = OneLineAvatarIconListItem(
                 text=name,
                 on_release=self.move_to_chat
@@ -87,14 +89,32 @@ class home(MDScreen):
 
         self.add_widget(layout)
     
-    def move_to_chat(self,*args):
+    def move_to_chat(self,instance):
+        friend_name = instance.text
+        self.manager.get_screen("chatting").friend_name = friend_name
         self.manager.current = "chatting"
+    def move_to_all_users(self,*args):
+        self.manager.current = "all_users"
+    def refresh_friend_list(self):
+        self.list.clear_widgets()
+
+        for name in self.friends:
+            item = OneLineAvatarIconListItem(
+                text=name,
+                on_release=self.move_to_chat
+            )
+            icon = IconLeftWidget(icon="account")
+            item.add_widget(icon)
+            self.list.add_widget(item)
+
 
 class chatting(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         name = "chatting"
+
+        self.friend_name = ""
 
         layout = MDFloatLayout()
 
@@ -133,7 +153,15 @@ class chatting(MDScreen):
             on_release=self.back_to_chat_list
         )
 
+        self.header = MDLabel(
+            text = "",
+            halign="center",
+            font_style="H5",
+            pos_hint={"center_x":0.5,"center_y":0.3}
+        )
+
         friend_info.add_widget(back_btn)
+        friend_info.add_widget(self.header)
 
         main_card.add_widget(friend_info)
 
@@ -147,7 +175,95 @@ class chatting(MDScreen):
 
     def back_to_chat_list(self,*args):
         self.manager.current = "home"
+    
+    def on_pre_enter(self, *args):
+        self.header.text = self.friend_name
 
+class all_users(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        name = "all_users"
+
+        layout = MDFloatLayout()
+
+        main_card = MDCard(
+            size_hint = (1,1),
+            md_bg_color = get_color_from_hex("#3B516E")
+        )
+
+        header_text = MDLabel(
+            text = "New Friends",
+            pos_hint = {'center_x':0.5,"center_y":0.95},
+            halign="center",
+            font_style="H4",
+            bold=True,
+            theme_text_color="Custom",
+            text_color=get_color_from_hex("#A4A5A6") 
+        )
+
+        back_btn = MDIconButton(
+            icon = "arrow-left",
+            pos_hint={"center_x":0.08,"center_y":0.95},
+            theme_text_color="Custom",
+            text_color=get_color_from_hex("#A4A5A6"),
+            on_release=self.move_to_back
+        )
+
+        nxt_layer_card = MDCard(
+            size_hint = (1,0.88),
+            md_bg_color = get_color_from_hex("#A4A5A6") 
+        )
+
+        scroll = MDScrollView()
+
+        self.list = MDList()
+
+        temp_friends = ["Herambha","Karthikeya","Guptha","Gayathri","Eswara Rao","Namratha"]
+
+        self.friends = ["self(You)"]
+
+        for name in temp_friends:
+            if name not in self.friends:
+                item = OneLineAvatarIconListItem(
+                    text=name,
+                )
+                icon = IconLeftWidget(icon="account")
+                item.add_widget(icon)
+
+                add_btn = IconRightWidget(
+                    icon = "account-plus",
+                    on_release=lambda x, n=name: self.add_friend(n)
+                )
+                item.add_widget(add_btn)
+                self.list.add_widget(item)
+        
+        scroll.add_widget(self.list)
+        nxt_layer_card.add_widget(scroll)
+
+
+        main_card.add_widget(back_btn)
+        main_card.add_widget(header_text)
+        
+
+        layout.add_widget(main_card)
+        layout.add_widget(nxt_layer_card)
+
+        self.add_widget(layout)
+    def move_to_back(self,*args):
+        self.manager.current = "home"
+    
+    def add_friend(self,friend_name):
+        if friend_name not in self.friends:
+            self.friends.append(friend_name)
+            friend_screen = self.manager.get_screen("home")
+            friend_screen.friends.append(friend_name)
+            friend_screen.refresh_friend_list()
+            self.manager.current = "home"
+            toast("Friend Added Successfully 👍")
+        else:
+            toast("Friend Already Exists 💀")
+        
 
 
 
@@ -156,6 +272,7 @@ class Example(MDApp):
         sm = MDScreenManager()
         sm.add_widget(home(name="home"))
         sm.add_widget(chatting(name="chatting"))
+        sm.add_widget(all_users(name="all_users"))
         return sm
 
 if __name__ == "__main__":

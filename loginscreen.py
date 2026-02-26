a5232912e9cc492ce6269eec254d8bba7f99e3dc
Herambha1226 <pallapothugayathri23@gmail.com>
2026-02-26 21:16:27 +0530

    Backend Flask integreted now for login

--- loginscreen.py
+++ loginscreen.py
@@ -1,13 +1,19 @@
 from kivymd.uix.backdrop.backdrop import MDFloatLayout
 from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
-from kivymd.uix.button import MDIconButton,MDRaisedButton,MDTextButton
+from kivymd.uix.button import MDIconButton,MDRaisedButton,MDTextButton,MDFlatButton
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.relativelayout import MDRelativeLayout
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.textfield import MDTextField
 from kivy.utils import get_color_from_hex
+from kivymd.toast import toast
+from mainscreen import home,chatting,all_users
+import requests
+
+url = "http://127.0.0.1:5000"
 
 class Start_Screen(MDScreen):
     def __init__(self, *args, **kwargs):
@@ -140,7 +146,8 @@ class Login_Screen(MDScreen):
             pos_hint={"X":0.07,"center_y":0.85}
         )
 
-        user_input = MDTextField(
+        self.user_input = MDTextField(
+            
             mode="rectangle",
             icon_left="account",
             text_color_focus= get_color_from_hex("#3B516E"),
@@ -157,7 +164,7 @@ class Login_Screen(MDScreen):
             bold=True,
             pos_hint={"X":0.07,"center_y":0.55}
         )
-        pass_input = MDTextField(
+        self.pass_input = MDTextField(
             mode="rectangle",
             icon_left="eye",
             text_color_focus= get_color_from_hex("#3B516E"),
@@ -183,7 +190,8 @@ class Login_Screen(MDScreen):
             md_bg_color=get_color_from_hex("#3B516E"),
             theme_text_color = "Custom",
             text_color = get_color_from_hex("#A4A5A6"),
-            size_hint=(1,0.1)
+            size_hint=(1,0.1),
+            on_release=self.login_user
         )
 
         signup_layout = MDBoxLayout(
@@ -216,9 +224,9 @@ class Login_Screen(MDScreen):
         inner_layout = MDFloatLayout()
         inner_layout.add_widget(log_label)
         inner_layout.add_widget(user_name)
-        inner_layout.add_widget(user_input)
+        inner_layout.add_widget(self.user_input)
         inner_layout.add_widget(user_password)
-        inner_layout.add_widget(pass_input)
+        inner_layout.add_widget(self.pass_input)
         inner_layout.add_widget(label2)
         inner_layout.add_widget(button)
         inner_layout.add_widget(signup_layout)  # SignUp buttton for the conection of signup screen
@@ -238,6 +246,31 @@ class Login_Screen(MDScreen):
         self.manager.current = "signup"
     def go_to_back(self,*args):
         self.manager.current = "start"
+    
+    def login_user(self,instance):
+        login_url = "http://127.0.0.1:5000/user_login"
+        user_name = self.user_input.text.strip()
+        user_pass = self.pass_input.text.strip()
+        if not user_name or not user_pass:
+            toast("Fill all textfields")
+            return
+        data = {
+            "user_name" : user_name,
+            "user_password" : user_pass
+        }
+        try:
+            resopnse = requests.post(login_url,json=data)
+            result = resopnse.json()
+            if result["message"] == "User Successfully Login.":
+                toast("Login Succssfully")
+                self.manager.current = "home"
+
+        except Exception as e:
+            toast("There is a problem in login !")
+    
+    
+
+            
 
 class signup_Screen(MDScreen):
     def __init__(self, *args, **kwargs):
@@ -298,14 +331,26 @@ class signup_Screen(MDScreen):
             pos_hint={"X":0.07,"center_y":0.85}
         )
 
-        email_input = MDTextField(
+        email_layout = MDRelativeLayout(
+            size_hint=(1, None),
+            height="60dp",
+            pos_hint={"center_x": 0.5, "center_y": 0.7}
+        )
+
+        self.email_input = MDTextField(
+            id="email_field",
             mode="rectangle",
             icon_left="mail",
             text_color_focus= get_color_from_hex("#3B516E"),
             icon_left_color_focus = get_color_from_hex("#3B516E"),
             line_color_focus = get_color_from_hex("#3B516E"),
-            pos_hint={"center_x":0.5,"center_y":0.7},
-              
+            pos_hint={"center_x":0.5,"center_y":0.42}
+        )
+
+        otp_button = MDFlatButton(
+            text="GET OTP",
+            pos_hint={"right": 1, "center_y": 0.5},
+            on_release=self.send_otp   # your function here
         )
         email_otp = MDLabel(
             text="OTP",
@@ -316,7 +361,8 @@ class signup_Screen(MDScreen):
             bold=True,
             pos_hint={"X":0.07,"center_y":0.55}
         )
-        otp_input = MDTextField(
+        self.otp_input = MDTextField(
+            id="OTP_field",
             mode="rectangle",
             icon_left="numeric",
             text_color_focus= get_color_from_hex("#3B516E"),
@@ -343,16 +389,18 @@ class signup_Screen(MDScreen):
             theme_text_color = "Custom",
             text_color = get_color_from_hex("#A4A5A6"),
             size_hint=(1,0.1),
-            on_release=self.go_to_next
+            on_release=self.check_otp
         )
 
-        
+        email_layout.add_widget(self.email_input)
+        email_layout.add_widget(otp_button)
+
         inner_layout = MDFloatLayout()
         inner_layout.add_widget(signup_label)
         inner_layout.add_widget(user_email)
-        inner_layout.add_widget(email_input)
+        inner_layout.add_widget(email_layout)
         inner_layout.add_widget(email_otp)
-        inner_layout.add_widget(otp_input)
+        inner_layout.add_widget(self.otp_input)
         inner_layout.add_widget(label2)
         inner_layout.add_widget(button)
         
@@ -370,11 +418,59 @@ class signup_Screen(MDScreen):
 
     def go_to_next(self,*args):
         self.manager.current = "user_create"
+    def send_otp(self,instance):
+        email = self.email_input.text
+
+        if not email:
+            toast("Enter email first !")
+            return
+        otp_url = "http://127.0.0.1:5000/verification"
+
+        data = {
+            "user_email" : email
+        }
+
+        try:
+            response = requests.post(otp_url,json=data)
+            result = response.json()
+            toast(result["message"])
+        except Exception as e:
+            toast("Server not running !")
+    
+    def check_otp(self,instance):
+        otp_url = "http://127.0.0.1:5000/verification2"
+        otp = self.otp_input.text.strip()
+        self.email = self.email_input.text.strip()
+        
+        screen_two = self.manager.get_screen("user_create")
+        screen_two.user_email = self.email
+
+        if not otp:
+            toast("Enter OTP")
+            return
+        data = {
+            "email" : self.email,
+            "otp" : otp
+        }
+
+        try:
+            response = requests.post(otp_url,json=data)
+            result = response.json()
+
+            toast(result["message"])
+            if result["message"] == "Email Verified":
+                self.go_to_next()
+
+        except Exception as e:
+            toast("Server not running")
+
 
 
 class new_user_screen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.user_email = ""
 
         self.name = "user_create"
 
@@ -431,7 +527,7 @@ class new_user_screen(MDScreen):
             pos_hint={"X":0.07,"center_y":0.85}
         )
 
-        username_input = MDTextField(
+        self.username_input = MDTextField(
             mode="rectangle",
             icon_left="account",
             text_color_focus= get_color_from_hex("#3B516E"),
@@ -449,7 +545,7 @@ class new_user_screen(MDScreen):
             bold=True,
             pos_hint={"X":0.07,"center_y":0.55}
         )
-        pass_input = MDTextField(
+        self.pass_input = MDTextField(
             mode="rectangle",
             icon_left="lock",
             text_color_focus= get_color_from_hex("#3B516E"),
@@ -475,16 +571,17 @@ class new_user_screen(MDScreen):
             md_bg_color=get_color_from_hex("#3B516E"),
             theme_text_color = "Custom",
             text_color = get_color_from_hex("#A4A5A6"),
-            size_hint=(1,0.1)
+            size_hint=(1,0.1),
+            on_release=self.create_user
         )
 
         
         inner_layout = MDFloatLayout()
         inner_layout.add_widget(signup_label)
         inner_layout.add_widget(user_Name)
-        inner_layout.add_widget(username_input)
+        inner_layout.add_widget(self.username_input)
         inner_layout.add_widget(user_pass)
-        inner_layout.add_widget(pass_input)
+        inner_layout.add_widget(self.pass_input)
         inner_layout.add_widget(label2)
         inner_layout.add_widget(button)
         
@@ -499,6 +596,34 @@ class new_user_screen(MDScreen):
         self.add_widget(layout)
     def go_to_back(self,*args):
         self.manager.current = "signup"
+    def create_user(self,instance):
+        create_user_url = "http://127.0.0.1:5000/user_create"
+        user_name =  self.username_input.text.strip()
+        user_pass = self.pass_input.text.strip()
+        if not user_name or not user_pass:
+            toast("Fill All fileds First !")
+            return
+        data = {
+            "user_email" : self.user_email,
+            "user_name" : user_name,
+            "user_password" : user_pass
+        }
+        print(self.user_email)
+        try:
+            response = requests.post(create_user_url,json=data)
+            result = response.json()
+            toast(result["message"])
+            if result["message"] == "user created successfully.":
+                toast("Successfully login !")
+                self.go_to_login()
+        except Exception as e:
+            toast("There is a Problem in Server.")
+    
+    def go_to_login(self,*args):
+        self.manager.current = "login"
+
+
+
         
 
 
@@ -512,6 +637,9 @@ class Example(MDApp):
         sm.add_widget(Login_Screen(name="login"))
         sm.add_widget(signup_Screen(name="signup"))
         sm.add_widget(new_user_screen(name="user_create"))
+        sm.add_widget(home(name="home"))
+        sm.add_widget(chatting(name="chatting"))
+        sm.add_widget(all_users(name="all_users"))
 
         
         return sm
